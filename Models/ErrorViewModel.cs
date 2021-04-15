1745a1cd2d4f03ccaa6e59455bf1547f40b79034
Celso de Ávila Ramos <37908497+celsodeavila@users.noreply.github.com>
2021-04-15 19:49:17 -0300

    Upload inicial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Aula_Github1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
